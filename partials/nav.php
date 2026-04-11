8d17372d3c16b4f9b916c11b75552de5db6890d3
George Cruz-Medina <gec23@njit.edu>
2026-04-11 03:23:23 -0400

    added helper functions and templating for navigation

---
+++ partials/nav.php
@@ -0,0 +1,49 @@
+<?php
+require(__DIR__."/../lib/functions.php"); 
+?>
+<nav> 
+    <ul>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+</nav>
+<?php
+//include functions here so we can have it on every page that uses the nav bar
+//that way we don't need to include so many other files on each page
+//nav will pull in functions and functions will pull in db
+
+// checking to see if domain has a port number attached (localhost)
+$domain = $_SERVER["HTTP_HOST"];
+if (strpos($domain, ":")) {
+    // strip the port number if present
+    $domain = explode(":", $domain)[0];
+}
+// used for public hosting like heroku
+if ($domain != "localhost") {
+    session_set_cookie_params([
+        "lifetime" => 60 * 60, // this is cookie lifetime, not session lifetime
+        "path" => "/project", // path to restrict cookie to; match your project folder (case sensitive)
+        "domain" => $domain, // domain to restrict cookie to
+        "secure" => true, // https only
+        "httponly" => true, // javascript can't access
+        "samesite" => "lax" // helps prevent CSRF, but allows normal navigation
+    ]);
+}
+session_start();
+require(__DIR__."/../lib/functions.php");
+?>
+<nav>
+    <ul>
+        <?php if (is_logged_in()) : ?>
+            <li><a href="landing.php">Home</a></li>
+        <?php endif; ?>
+        <?php if (!is_logged_in()) : ?>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+        <?php endif; ?>
+        <?php if (is_logged_in()) : ?>
+            <li><a href="logout.php">Logout</a></li>
+        <?php endif; ?>
+    </ul>
+</nav>
