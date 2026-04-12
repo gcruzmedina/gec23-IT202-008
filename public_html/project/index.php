22f5c9ef71a4fcd1f9c4a4ab2f2afa87acda916d
George Cruz-Medina <gec23@njit.edu>
2026-04-12 16:42:43 -0400

    asumadreweon

--- public_html/project/index.php
+++ public_html/project/index.php
@@ -1,3 +1,4 @@
+
 <style>
     /* TODO can replace the CSS as you wish or replace with bootstrap in Milestone2+ topics */
 .jumbotron {
@@ -43,14 +44,16 @@
     color: #fff;
 }
 </style>
-<?php require(__DIR__ . "/../../lib/functions.php"); ?>
+<?php
+require(__DIR__ ."/../../lib/functions.php");
+?>
 <div class="jumbotron">
     <!-- TODO replace your name -->
     <h1 class="display-4">Welcome to Matt's project</h1>
     <!-- TODO change Date/Semester accordingly -->
     <p class="lead">This is for the Summer semester of IT202 2025</p>
     <p>
-        <a class="btn btn-primary" href="<?php echo $BASE_PATH;?>/register.php" class="btn btn-primary">Register</a>
-        <a class="btn btn-secondary" href="<?php echo $BASE_PATH;?>/login.php" class="btn btn-secondary">Login</a>
+        <a class="btn btn-primary" href="<?php get_url('register.php', true);?>" class="btn btn-primary">Register</a>
+        <a class="btn btn-secondary" href="<?php get_url('login.php', true);?>" class="btn btn-secondary">Login</a>
     </p>
 </div>
--- public_html/project/landing.php
+++ public_html/project/landing.php
@@ -1,35 +1,13 @@
 <?php
-
 require(__DIR__ . "/../../partials/nav.php");
+error_log("Session: " . var_export($_SESSION, true));
 ?>
 <h1>Landing Page</h1>
-<?php
-error_log("Session: " . var_export($_SESSION, true));
-if (is_logged_in()) {
-    // Note, we don't need to use `se()` here since `get_user_email()` uses it internally
-    echo "Welcome, " . get_user_email();
-} else {
-    echo "You're not logged in";
-}
-?>
+
+<?php if(is_logged_in(true)):?>
+    <p>Welcome, <?php echo get_username() ?>!</p>
+<?php endif;?>
 
 <?php
 require(__DIR__."/../../partials/flash.php");
-require(__DIR__."/../../partials/nav.php");
-?>
-<h1>Landing Page</h1>
-<?php
-error_log("Session: ". var_export($_SESSION, true));
-
-if(isset($_SESSION["user"], $_SESSION["user"]["email"])){
- echo "Welcome, ";
- // Always escape user data before outputting it to prevent XSS, even if you trust your session data
- echo se($_SESSION["user"]["email"]);
-if(is_logged_in()) {
-    echo "Welcome, " . get_user_email();
-}
-else{
-  echo "You're not logged in";
-}
-
 ?>
